eba5152d64168537e1c2125898d5d43da30e70d3
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-04-07 16:03:51 -0500

    mvo

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const expressRouter = require('../expressRouter');
+const server = express();
+
+server.use(express.json());
+server.use('/api/posts', expressRouter);
+server.get('/', (req, res) => {
+	res.send('SUCCESS');
+});
+
+module.exports = server;
